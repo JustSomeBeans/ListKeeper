09ac4fde343d4967b7f9dabe8597b167345c5b0f
JustSomeBeans <102830580+JustSomeBeans@users.noreply.github.com>
2022-11-19 13:16:36 -0800

    Update Listkeeper.py

--- Listkeeper.py
+++ Listkeeper.py
@@ -7,16 +7,16 @@ def inpt(): #console checking function
     global order, lit, inp, turn, count #declaring global variables
     bypass = 0 #way of bypassing bullshit code order idk if i need this left it in anyway
     inp = str(input("\n")) #gets user command input
-    if inp[0:5] == "/help": #detects /help in inp string
+    if inp.lower[0:5] == "/help": #detects /help in inp string
         print("/add [username] - adds a new user to the rotation\n/new - adds a new player to the rototation behind the player's who turn it is\n/next - tells you which user is next\n/order - gives you the rotation order\n/remove [username] - removes a user from the rotation")
         #line above listing the available commands
-    if inp[0:5] == "/add ": #detects /add in inp string
+    if inp.lower[0:5] == "/add ": #detects /add in inp string
         lit.append(inp.replace("/add ", "")) #adds the new user to the list at the end
         count = len(lit) #updates the player count
         print("Added " + inp.replace("/add ", "")) #console response
         print("Total playe(s) is now: " + str(count)) #console response
 
-    if inp[0:5] == "/new ": #detects /new in inp string
+    if inp.lower[0:5] == "/new ": #detects /new in inp string
         lit.insert((turn - 2), inp.replace("/new ", "")) #inserts the new player into the position behind the player's who turn it is
         count = len(lit) #updates player count
         print("Added " + inp.replace("/new ", "")) #console response
@@ -36,14 +36,14 @@ def inpt(): #console checking function
             turn = turn + 1 #updates turn
         bypass = 1
 
-    if inp[0:8] == "/remove ":
+    if inp.lower[0:8] == "/remove ":
         lit.remove(inp.replace("/remove ", "")) #removes player frrom the list
         count = count - 1 #updates count
         turn = turn - 1 #preserves turn order
         print("Removed " + inp.replace("/remove", "")) #console response
         print("Total player(s) is now " + str(count)) #console response
 
-    if inp[0:6] == "/order": #detects /order command
+    if inp.lower[0:6] == "/order": #detects /order command
         print(lit) #console response
     inpt()
 inpt()
