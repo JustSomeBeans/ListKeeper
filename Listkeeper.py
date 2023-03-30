91be4fe52a31f4f7a9c5ff4dab7899ba6cdfbcf2
JustSomeBeans <102830580+JustSomeBeans@users.noreply.github.com>
2023-03-29 20:10:52 -0700

    Update Listkeeper.py

--- Listkeeper.py
+++ Listkeeper.py
@@ -1,64 +1,114 @@
-print("Welcome to list keeper by JustSomeBeans!\ntype /help for a list of commands\n")
-order = 1 #setting global variables
-count = 1
-turn = 1
-lit = []
-verify = 0 
-def inpt(): #console checking function
-    global order, lit, inp, turn, count, verify #declaring global variables
-    bypass = 0 #way of bypassing bullshit code order idk if i need this left it in anyway
-    inp = str(input("\n")) #gets user command input
-    if inp[0:5] == "/help": #detects /help in inp string
-        print("/add [username] - adds a user to the rotation\n/new [username] - adds a new player to the rototation behind the player's who turn it is\n/next - tells you which user is next\n/order - gives you the rotation order\n/remove [username] - removes a user from the rotation")
-        #line above listing the available commands
-    if inp[0:5] == "/add ": #detects /add in inp string
-        if verify == 0:
-            lit.append(inp.replace("/add ", "")) #adds the new user to the list at the end
-            count = len(lit) #updates the player count
-            print("Added " + inp.replace("/add ", "")) #console response
-            print("Total playe(s) is now: " + str(count)) #console response
-        if verify != 0:
-            yn1 = str(input("Using /add will add " + inp.replace("/add", "") + "to the bottom of the list instead of behind the current player causing them to cut the line\nUsing /add after rotation has started may cause listing error\nAre you sure you want to use /add instead of /new? (y/n)"))
-            if yn1[0] == "y":
-                lit.append(inp.replace("/add ", "")) #adds the new user to the list at the end
-                count = len(lit) #updates the player count
-                print("Added " + inp.replace("/add ", "")) #console response
-                print("Total playe(s) is now: " + str(count)) #console response
-            if yn1[0] == "n":
-                print("aborted adding " + inp.replace("/add", ""))
-    if inp[0:5] == "/new ": #detects /new in inp string
-        lit.insert((turn - 2), inp.replace("/new ", "")) #inserts the new player into the position behind the player's who turn it is
-        count = len(lit) #updates player count
-        print("Added " + inp.replace("/new ", "")) #console response
-        print("Total playe(s) is now: " + str(count)) #console response
-        if turn != count: #preserving who's turn it is
-            turn = turn + 1 #updating the turn if the roation is not at the end
-        elif turn == count:
-            turn = 1 #updating the turn if the rotation is at the end
-        
-    if inp[0:5] == "/next": #detects /next
-        if turn == count: #makes sure the turn order loops when it gets to the end of the list
-            print("it is " + lit[(turn - 1)] + "'s turn\n") #console response
-            turn = 1 #loops turn to start of the list
-            bypass = 1 #enables the bypass of the next logic statement
-        elif turn != count and bypass == 0: #prints who's turn it is and updates who's next
-            print("It is " + lit[(turn - 1)] + "'s turn\n") #console repsonse
-            turn = turn + 1 #updates turn
-        bypass = 0
-        verify = 1
+# makes an empty list to hold players
+players = []
 
-    if inp[0:8] == "/remove ":
-        yn = str(input("Are you sure you want to remove " + inp.replace("/remove ", "") + "? | (y/n)\n"))
-        if yn[0] == "y":
-            lit.remove(inp.replace("/remove ", "")) #removes player frrom the list
-            count = count - 1 #updates count
-            turn = turn - 1 #preserves turn order
-            print("Removed " + inp.replace("/remove", "")) #console response
-            print("Total player(s) is now " + str(count)) #console response
-        if yn[0] == "n":
-            print("Aborted removal of " + inp.replace("/remove ", ""))
+# defines the function to add a player to the list
+def add_player():
+    # prompts user for player name
+    name = input("Enter the player's name: ")
+    # adds player to the list
+    players.append(name)
+    # prints response message
+    print(f"{name} has been added to the list.\n")
 
-    if inp[0:6] == "/order": #detects /order command
-        print(lit) #console response
-    inpt()
-inpt()
+# defines the function to remove a player from the list
+def remove_player():
+    # prompts user for the player's name
+    name = input("Enter the player's name: ")
+    # checks if player is in the list
+    if name.lower() in [p.lower() for p in players]:
+        # asks user before removing player
+        confirm = input(f"Are you sure you want to remove {name}? (y/n)\n")
+        if confirm.lower() == "y":
+            # removes player from the list
+            players.remove([p for p in players if p.lower() == name.lower()][0])
+            # prints response message
+            print(f"{name} has been removed.\n")
+        else:
+            # prints message confirming the player was not removed
+            print(f"{name} was not removed.\n")
+    else:
+        # prints message if player isn't not found in the list
+        print(f"{name} was not found.\n")
+
+# defines the function to go to the next player in the list
+def next_player():
+    # checks if there are any players in the list
+    if len(players) == 0:
+        # prints message if there are no players
+        print("No players found.\n")
+    else:
+        # prints message showing whose turn it is
+        print(f"It is {players[0]}'s turn.\n")
+        # moves the first player to the end of the list
+        players.append(players.pop(0))
+
+# defines function to display the current list of players
+def view_players():
+    # checks if there are any players in the list
+    if len(players) == 0:
+        # prints response message if there are no players in the list
+        print("No players found.\n")
+    else:
+        # prints the list of players
+        print("Current players in rotation:")
+        for player in players:
+            print(player)
+        print("\n")
+# defines the function to add a player to the back of the rotation
+def add_to_rotation():
+    # prompts user for player name
+    name = input("Enter the player's name: ")
+    # adds player to the back of the list
+    players.append(name)
+    # print response message
+    print(f"{name} has been added to the back of the rotation.\n")
+
+# defines the function to display the help menu
+def help_menu():
+    # prints a list of available options
+    print("1. Add player - Adds a new player to the list")
+    print("2. Remove player - Removes a player from the list")
+    print("3. Next player - Advances to the next player in the rotation")
+    print("4. View players - Shows the list of players currently in rotation")
+    print("5. Add player to rotation - Adds a new player to the back of the rotation")
+    print("6. Help - Displays this help menu")
+    print("7. Quit - Exits the program\n")
+
+print("Welcome to Listkeeper 2.0 by JustSomeBeans!\nTo get started enter the number corripsonding to your selection\n")
+
+# main program loop
+while True:
+    # displays the main menu
+    print("1. Add player")
+    print("2. Remove player")
+    print("3. Next player")
+    print("4. View players")
+    print("5. Add player to rotation")
+    print("6. Help")
+    print("7. Quit")
+    # prompts the user for choice
+    choice = input("Enter your choice: ")
+    # calls defined function based on user choice
+    if choice == "1":
+        add_player()
+    elif choice == "2":
+        remove_player()
+    elif choice == "3":
+        next_player()
+    elif choice == "4":
+        view_players()
+    elif choice == "5":
+        add_to_rotation()
+    elif choice == "6":
+        help_menu()
+    elif choice == "7":
+        # asks user if they really want to quit
+        confirm = input("Are you sure you want to quit? (y/n)\n")
+        # quits program
+        if confirm.lower() == "y":
+            break
+        else:
+            print("Program will continue.\n")
+    # tells user if their input was invalid
+    else:
+        print("Invalid choice. Please try again.\n")
